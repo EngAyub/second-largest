75c82de2184dd7b2668c269bc50ac9101d8977eb
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 20:01:19 +0300

    Second largest done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function secondLargest(ak) {
+    ak.sort(function(a, b) {
+      return b - a;
+    });
+    return ak[1];
+  }
+  console.log(secondLargest([44, 55, 66, 79, 88])); // 79
+  console.log(secondLargest([10, 30, 86, 60, 90]));  //86
+  console.log(secondLargest([67, 22, 19, 12, 80]));  // 67
+
+
